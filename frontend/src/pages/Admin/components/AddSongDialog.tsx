72aac15c7edb628ee9e010f8a8d719c4a0cfa935
konrad3211 <konradpatla@gmail.com>
2026-05-15 20:47:12 +0200

    Update AddSongDialog.tsx

--- frontend/src/pages/Admin/components/AddSongDialog.tsx
+++ frontend/src/pages/Admin/components/AddSongDialog.tsx
@@ -16,7 +16,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { axiosInstance } from "@/lib/axios";
 import { useMusicStore } from "@/stores/useMusicStore";
 import { Plus, Upload } from "lucide-react";
 import { useRef, useState } from "react";
